32c9365b33211c0afdedab6259e46827d0c64aca
Kevin Jacob <kevinjacob2001@gmail.com>
2023-03-13 16:05:47 +0530

    Added the routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-function App() {
+// Components for protected routes
+
+// Components for unprotected routes
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
+
+// Components for authentication
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import ProfilePage from './components/ProfilePage';
+
+// A function to check if the user is authenticated
+const isAuthenticated = () => {
+  // Check if the user is logged in or not
+  // You can implement your own logic to check authentication
+  return false; // For example, always return true for demo purposes
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Unprotected routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        {/* Login and Signup routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Protected routes */}
+        <Route path="/dashboard" element={<ProtectedRoute component={Dashboard} />} />
+        <Route path="/profile" element={<ProtectedRoute component={ProfilePage} />} />
+
+        {/* 404 Not Found */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
+}
+
+// A component for protected routes
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    isAuthenticated() ? (
+      <Component {...rest} />
+    ) : (
+      <Navigate to="/login" replace />
+    )
+  )
 }
 
 export default App;
---
+++ src/components/ProfilePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>ProfilePage is here</div>
+  )
+}
+
+export default ProfilePage
